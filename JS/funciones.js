198dc956b24d648cdb1249fea958080a317e410f
Jaquelin <240743@utxicotepec.edu.mx>
2026-01-08 14:29:57 -0600

    Crear funciones js y estilos de encabezado

--- JS/funciones.js
+++ JS/funciones.js
@@ -1,3 +1,36 @@
 function saludar(){
     alert("Bienvenido a la Academia de T.I");
 }
+function suma(){
+    var valor1 = 24;
+    var valor2 = 13;
+    var resultado= valor1 + valor2;
+    alert ("Resultado de la suma es:"+resultado);
+}
+
+function resta(){
+    var valor1 = 15;
+    var valor2 = 5;
+    var resultado= valor1 - valor2;
+    alert ("Resultado de la resta es:"-resultado);
+}
+
+function multiplicacion(){
+    var valor1 = 20;
+    var valor2 = 9;
+    var resultado= valor1 * valor2;
+    alert ("Resultado de la multiplicacion es:"*resultado);
+}
+
+function division(){
+    var valor1 = 11;
+    var valor2 = 2;
+    var resultado= valor1 / valor2;
+    alert ("Resultado de la suma es:"/resultado);
+
+
+
+
+
+
+}

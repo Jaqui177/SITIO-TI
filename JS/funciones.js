e0148e5302c9a4a9807eb5b779fb1bd4724ddc59
unknown <240743@utxicotepec.edu.mx>
2026-01-13 11:35:49 -0600

    se agregaron tablas y se modificaron los apartados index y style eh imagenes

--- JS/funciones.js
+++ JS/funciones.js
@@ -5,28 +5,28 @@ function suma(){
     var valor1 = 24;
     var valor2 = 13;
     var resultado= valor1 + valor2;
-    alert ("Resultado de la suma es:"+resultado);
+    alert ("Resultado de la suma de:"+valor1+ "y" +valor2+ "es: " + resultado);
 }
 
 function resta(){
     var valor1 = 15;
     var valor2 = 5;
     var resultado= valor1 - valor2;
-    alert ("Resultado de la resta es:"-resultado);
-}
+    alert ("Resultado de la resta de:"+valor1+ "y" +valor2+ "es: " + resultado);
+    }
 
 function multiplicacion(){
     var valor1 = 20;
     var valor2 = 9;
     var resultado= valor1 * valor2;
-    alert ("Resultado de la multiplicacion es:"*resultado);
+    alert ("Resultado de la multiplicacion de:"+valor1+ "y" +valor2+ "es: " + resultado);
 }
 
 function division(){
     var valor1 = 11;
     var valor2 = 2;
     var resultado= valor1 / valor2;
-    alert ("Resultado de la suma es:"/resultado);
+    alert ("Resultado de la suma de:"+valor1+ "y" +valor2+ "es: " + resultado);
 
 
 
